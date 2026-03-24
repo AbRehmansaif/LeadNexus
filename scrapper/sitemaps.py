03cf7e1c1b8e8fbfdc73bb96f5c4adae958aed29
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2026-03-25 01:11:38 +0500

    seo the tools pages

--- scrapper/sitemaps.py
+++ scrapper/sitemaps.py
@@ -41,6 +41,11 @@ class ToolsViewSitemap(Sitemap):
         return [
             'seo:seo-tool-markdown-to-html',
             'seo:seo-tool-excel-to-csv',
+            'seo:seo-tool-utm-link-builder',
+            'seo:seo-tool-dns-config-generator',
+            'seo:seo-tool-cold-email-roi-calculator',
+            'seo:seo-tool-email-spam-word-checker',
+            'seo:seo-tool-email-warmup-calculator',
         ]
 
     def location(self, item):
